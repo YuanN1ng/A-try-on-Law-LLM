b1f4f28d61d1b9d34df556210806b3a4fbb22612
Yuan Ning <YuanNing.nn@gmail.com>
2025-03-19 20:41:58 +0800

    Update start.py

--- start.py
+++ start.py
@@ -2,39 +2,39 @@ from transformers import pipeline
 import torch
 from pprint import pprint
 
-# 创建pipeline
+
 pipe = pipeline("text-generation",
                 model="MaziyarPanahi/calme-3.1-llamaloi-3b",
-                device=0,  # 使用 GPU
+                device=0,  
                 torch_dtype=torch.float16,
-                max_length= 1000)  # 设置为 FP16
+                max_length= 1000) 
 
-# 用于存储对话历史的变量
+# 用于存储对话历史
 conversation_history = []
 n=1
-# 循环进行交互式对话
+# 循环以进行交互式对话
 while True:
-    # 获取用户输入
+
 
     user_input = input("You: ")
 
-    if user_input.lower() == "exit":  # 退出对话
+    if user_input.lower() == "exit":  
         break
 
-    # 将用户输入添加到对话历史中
+    # 将对话历史添加到输入
     conversation_history.append({"role": "user", "content": user_input})
 
 
     input_for_model = conversation_history
 
-    # 使用pipeline生成模型回复
+  
     result = pipe(input_for_model)
 
 
-    # 直接提取生成的文本
+    # 直接提取文本
     assistant_reply = result[0]['generated_text'][n]['content']
 
-    # 打印模型的纯文本回复
+    # 打印纯文本回复
     print(f"Assistant: {assistant_reply}")
 
 
